a3a288aa3127e90b7d4a3b96ff52b9a5a4642e16
alexjeser <alexander.eser@me.com>
2015-12-10 22:21:36 +0100

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, '/Users/alexanderjohanneser/djangoman/blog/templates/post_list.html', {})
